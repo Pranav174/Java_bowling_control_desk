a17b2beca4e6fa4c2064435495c68754fefbffff
Pranav174 <pranav.goyal@research.iiit.ac.in>
2020-04-08 16:28:06 +0530

    random changes

--- code/drive.java
+++ code/drive.java
@@ -5,14 +5,14 @@ public class drive {
 
 	public static void main(String[] args) {
 
-		int numLanes = 3;
-		int maxPatronsPerParty=5;
+		int numLanes = 5;
+		int maxPatronsPerParty=6;
 
 		Alley a = new Alley( numLanes );
 		ControlDesk controlDesk = a.getControlDesk();
 
-		ControlDeskView cdv = new ControlDeskView( controlDesk, maxPatronsPerParty);
+		ControlDeskView cdv = new ControlDeskView(controlDesk, maxPatronsPerParty);
 		controlDesk.subscribe( cdv );
-
+		
 	}
 }
